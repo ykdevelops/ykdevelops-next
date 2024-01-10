3da9448cc787f93e7be6ca401bea67cfbd7abeeb
ykdevelops <ykdevelops@gmail.com>
2024-01-10 01:33:10 -0500

    feat: :zap: Installing Vercel SpeedInsights

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
