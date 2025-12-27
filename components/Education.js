677ff5b81c967d988d199a899f6142a07b4156fe
Youssef Khalil <youssefkhalil@Youssefs-Mac-mini.local>
2025-12-27 14:09:42 -0800

    Separate Education and Certifications into separate components, update design language to match WorkExperience, adjust Contact text sizes and colors, remove padding from pitchRightColumn on mobile

--- components/Education.js
+++ components/Education.js
@@ -2,15 +2,12 @@ import React, { Suspense } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-export default function EducationAndCerts() {
+export default function Education() {
   return (
-    <div className={styles.eduLayer}>
-      {/* =======================
-          EDUCATION SECTION
-      ======================= */}
-      <div className={styles.eduContainer}>
-        <div className={styles.eduTitleRow}>
-          <Suspense fallback={<div>Loading icon...</div>}>
+    <div className={styles.layer}>
+      <div className={styles.workContainer}>
+        <div className={styles.containerTitleRow}>
+          <Suspense fallback={<div className={styles['loading-image']}></div>}>
             <Image
               src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/education.png"
               alt="educationIcon"
@@ -23,141 +20,46 @@ export default function EducationAndCerts() {
         </div>
 
         {/* University of Ottawa (Current) */}
-        <div className={styles.eduItem}>
-          <Suspense fallback={<div>Loading logo...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/uottawa.png"
-              alt="uOttawaLogo"
-              width={150}
-              height={150}
-              className={styles.eduLogo}
-            />
-          </Suspense>
-          <div className={styles.eduInfo}>
-          <h2 className={styles.eduItemDegree}>Honours BSc Computer Science</h2>
-            <h1 className={styles.eduItemTitle}>
-              University of Ottawa
-            </h1>
-            
-            <h2 className={styles.eduItemDate}>Sep 2024 – Sep 2027</h2>
+        <div className={styles.workCompany}>
+          <div className={styles.workCompanyHeader}>
+            <Suspense fallback={<div className={styles['loading-image']}></div>}>
+              <Image
+                src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/uottawa.png"
+                alt="uOttawaLogo"
+                width={150}
+                height={150}
+                className={styles.workLogo}
+              />
+            </Suspense>
+            <div className={styles.workInfo}>
+              <h2 className={styles.eduItemDegree}>Honours BSc Computer Science</h2>
+              <h1 className={styles.eduItemTitle}>
+                University of Ottawa
+              </h1>
+              <h2 className={styles.eduItemDate}>Sep 2024 – Sep 2027</h2>
+            </div>
           </div>
         </div>
 
         {/* Algonquin College */}
-        <div className={styles.eduItem}>
-          <Suspense fallback={<div>Loading logo...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/algonquin.png"
-              alt="AlgonquinLogo"
-              width={150}
-              height={150}
-              className={styles.eduLogo}
-            />
-          </Suspense>
-          <div className={styles.eduInfo}>
-          <h2 className={styles.eduItemDegree}>Mobile Application Design & Development</h2>
-            <h1 className={styles.eduItemTitle}>
-              Algonquin College
-            </h1>
-            
-            <h2 className={styles.eduItemDate}>Sep 2019 – Apr 2022</h2>
-          </div>
-        </div>
-
-
-      </div>
-
-      {/* ================================
-          CERTIFICATIONS
-      ================================ */}
-      <div className={styles.eduContainer}>
-        <div className={styles.eduTitleRow}>
-          <Suspense fallback={<div>Loading icon...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/certificate.png"
-              alt="certIcon"
-              width={150}
-              height={150}
-              className={styles.sectionTitleIcon}
-            />
-          </Suspense>
-          <h1 className={styles.sectionTitle}>Certifications</h1>
-        </div>
-
-        {/* Google Cybersecurity Specialization */}
-        <div className={styles.eduItem}>
-          <Suspense fallback={<div>Loading logo...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/google.jpg"
-              alt="GoogleLogo"
-              width={150}
-              height={150}
-              className={styles.eduLogo}
-            />
-          </Suspense>
-          <div className={styles.eduInfo}>
-            <h1 className={styles.eduItemTitle}>Google Cybersecurity Specialization</h1>
-            <h2 className={styles.eduItemDate}>Issued Jan 2025</h2>
-
-            <a
-              href="https://coursera.org/share/5b806dc8790c26727c4f81e2d6a0f89c"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.eduLink}
-            >
-              Credential <Image src="/leave-website.svg" alt="external link" width={24} height={16} />
-            </a>
-          </div>
-        </div>
-
-        {/* AWS Certified Cloud Practitioner */}
-        <div className={styles.eduItem}>
-          <Suspense fallback={<div>Loading logo...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/aws.jpg"
-              alt="AWSLogo"
-              width={150}
-              height={150}
-              className={styles.eduLogo}
-            />
-          </Suspense>
-          <div className={styles.eduInfo}>
-            <h1 className={styles.eduItemTitle}>AWS Certified Cloud Practitioner</h1>
-            <h2 className={styles.eduItemDate}>Issued Oct 2023 – Expires Oct 2026</h2>
-            <a
-              href="https://www.credly.com/badges/c2c6ded9-a3f6-4461-9618-7ead3a2d43f9/linked_in_profile"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.eduLink}
-            >
-              Credential <Image src="/leave-website.svg" alt="external link" width={24} height={16} />
-            </a>
-          </div>
-        </div>
-
-        {/* OpenCV for Beginners */}
-        <div className={styles.eduItem}>
-          <Suspense fallback={<div>Loading logo...</div>}>
-            <Image
-              src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/openCV.png"
-              alt="OpenCVLogo"
-              width={150}
-              height={150}
-              className={styles.eduLogo}
-            />
-          </Suspense>
-          <div className={styles.eduInfo}>
-            <h1 className={styles.eduItemTitle}>OpenCV for Beginners</h1>
-            <h2 className={styles.eduItemDate}>Issued May 2023</h2>
-
-            <a
-              href="https://courses.opencv.org/certificates/f15b061ca3b24324b49cd000d15a2e40"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.eduLink}
-            >
-              Credential <Image src="/leave-website.svg" alt="external link" width={24} height={16} />
-            </a>
+        <div className={styles.workCompany}>
+          <div className={styles.workCompanyHeader}>
+            <Suspense fallback={<div className={styles['loading-image']}></div>}>
+              <Image
+                src="https://ykdevelops.s3.us-east-2.amazonaws.com/education/algonquin.png"
+                alt="AlgonquinLogo"
+                width={150}
+                height={150}
+                className={styles.workLogo}
+              />
+            </Suspense>
+            <div className={styles.workInfo}>
+              <h2 className={styles.eduItemDegree}>Mobile Application Design & Development</h2>
+              <h1 className={styles.eduItemTitle}>
+                Algonquin College
+              </h1>
+              <h2 className={styles.eduItemDate}>Sep 2019 – Apr 2022</h2>
+            </div>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Intro from '../components/Intro'
 import Education from '../components/Education'
+import Certifications from '../components/Certifications'
 import Pitch from '../components/Pitch'
 import WorkExperience from '../components/WorkExperience'
 import Studio from '../components/sideProjects/Studio'
@@ -56,6 +57,7 @@ export default function Home() {
       <Pitch />
       <WorkExperience />
       <Education />
+      <Certifications />
       <Contact />
     </div>
   )
